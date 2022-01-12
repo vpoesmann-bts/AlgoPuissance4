147799224dbe3106e9cf6523af3c71858ecee9aa
vpoesmann-bts <vpoesmann-bts@gmail.com>
2022-01-12 10:37:41 +0100

    Create 4 in a row html grid

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
 console.log("Salut")
+
+let grille : HTMLElement = document.getElementById("grille")
+
+console.log(grille)
+
+for (let i: number = 0 ; i < 6 ; i++) {
+  let colonne: HTMLElement = document.createElement("div");
+  for (let j: number = 0 ; j < 7 ; j++) {
+    let newDiv : HTMLElement = document.createElement("div");
+    colonne.appendChild(newDiv)
+  }
+  grille.appendChild(colonne)
+}
